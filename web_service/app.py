943e00c3803d87cc806c3b527f21283c278319db
truondin <dannenr@gmail.com>
2024-12-07 12:43:56 +0200

    Combine Web UI with web service

--- web_service/app.py
+++ web_service/app.py
@@ -1,9 +1,12 @@
+import random
 
 from flask import Flask, render_template, request
+from datetime import datetime
 
 import threading
 import mqtt as mqtt_handler
 import db
+import json
 
 
 app = Flask(__name__)
@@ -57,7 +60,30 @@ def startThreads():
         return "Starting threads"
 
 
+
+@app.route('/<string:page_name>/')
+def static_page(page_name):
+    nID = request.args.get('nID')
+    return render_template('%s.html' % page_name,nID=nID)
+
+
+@app.route('/robots/<rID>/latest', methods=['GET'])
+def getRobotLatestState(rID):
+    possibleStates = ["ACTIVE", "IDLE", "DOWN"]
+    currentState = random.choice(possibleStates)
+
+    now = datetime.now()
+
+
+    current_time = now.strftime("%H:%M:%S")
+    return json.dumps({"currentState": currentState, "lastTimeConnected": current_time})
+
+
+
 if __name__ == '__main__':
-    startThreads()
-    db.create_db()
-    app.run()
+    try:
+        startThreads()
+        db.create_db()
+        app.run()
+    finally:
+        print("Stopping web service")
